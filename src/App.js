c1ccc66b026c45042cb8d9789b91013a7b3df635
Richard Hung <richardhung111@gmail.com>
2018-12-10 23:37:27 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Stuff from './components/Stuff';
+import Contact from './components/Contact';
+import './scss/component-app.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div className="component--app">
+          <h1>Pokedex</h1>
+          <ul className="header">
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/stuff">Stuff</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">Contact</NavLink>
+            </li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Home} />
+            <Route path="/stuff/:name?" component={Stuff} />
+            <Route path="/contact" component={Contact} />
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
